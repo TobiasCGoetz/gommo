13d7f1f753b512eddafd141db06539276fd39b65
Tobias Götz <TobiasCarstenGoetz@GMail.com>
2021-09-10 19:10:49 +0200

    Beginning work on playerList

--- main.go
+++ main.go
@@ -56,15 +56,23 @@ func tick() {
 	spread()
 }
 
+func printSlice(s []Player) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	//var forest Tile = Forest
 	//var food Card = Food
-	//var me Player = Player{"first", 13, 42, [4]Card{Food, Wood, Wood, None}}
+	var me Player = Player{"me", 13, 42, North, Weapon, [4]Card{Food, Wood, Wood, None}}
 	//fmt.Println(forest.toString())
 	//fmt.Println(food.toString())
 	//fmt.Println(me.toString())
 	var gameMap [mapWidth][mapHeight]Tile
+	var playerList []Player
+	printSlice(playerList)
+	playerList = append(playerList, me)
+	printSlice(playerList)
 	initMap(&gameMap)
 	printMap(&gameMap)
 }
