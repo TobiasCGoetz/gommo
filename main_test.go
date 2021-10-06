01e594d2a8238e24e16d7a252c5ae8a2c6e04052
TGoz <tobiascarstengoetz@gmail.com>
2021-10-06 23:35:20 +0200

    Add TestCreateCityList

--- main_test.go
+++ main_test.go
@@ -15,3 +15,27 @@ func TestInitMap (t *testing.T) {
 		}
 	}
 }
+
+func fakeInitMap(gMap *[mapWidth][mapHeight]*Tile) {
+	for a, column := range gMap {
+		for b, _ := range column {
+			var tile = Tile{City, 0}
+			gMap[a][b] = &tile
+		}
+	}
+}
+
+func TestCreateCityList(t *testing.T) {
+	var gameMap [mapWidth][mapHeight]*Tile
+	fakeInitMap(&gameMap)
+	var cityList = createCityList(&gameMap)
+	var count = 0
+	for x := 0; x < mapWidth; x++ {
+		for y := 0; y < mapHeight; y++ {
+			if cityList[count].x != x || cityList[count].y != y {
+				t.Errorf("We're missing [%d][%d] in the city list", x, y)
+			}
+			count++
+		}
+	}
+}
