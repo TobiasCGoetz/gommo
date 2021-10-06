1b15f91b1732d42700122a8272433394056e5d50
TGoz <tobiascarstengoetz@gmail.com>
2021-10-06 23:55:32 +0200

    Added TestMove

--- main_test.go
+++ main_test.go
@@ -39,3 +39,40 @@ func TestCreateCityList(t *testing.T) {
 		}
 	}
 }
+
+//TODO: Figure out if positive Y == North is stupid
+func TestMove(t *testing.T) {
+	var playerX = 5
+	var playerY = 5
+	var testPlayer = Player{
+		id:        "test",
+		x:         playerX,
+		y:         playerY,
+		direction: North,
+		play:      Dice,
+		consume:   Wood,
+		discard:   Wood,
+		cards:     [5]Card{Wood, Wood, Food, None, None},
+		alive:     true,
+	}
+	var testArray = []*Player{&testPlayer}
+	move(&testArray)
+	if testPlayer.y != playerY+1 {
+		t.Errorf("Move north failed.")
+	}
+	testPlayer.direction = South
+	move(&testArray)
+	if testPlayer.y != playerY {
+		t.Errorf("Move south failed.")
+	}
+	testPlayer.direction = East
+	move(&testArray)
+	if testPlayer.x != playerX+1 {
+		t.Errorf("Move east failed.")
+	}
+	testPlayer.direction = West
+	move(&testArray)
+	if testPlayer.x != playerX {
+		t.Errorf("Move west failed.")
+	}
+}
