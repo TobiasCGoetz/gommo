216f31870cf27e04728a86839835627d9f81d9ba
Tobias Götz <tobias.goetz@wirtschaft.uni-giessen.de>
2022-03-27 18:02:13 +0200

    Add missing params to config

--- config.go
+++ config.go
@@ -5,5 +5,7 @@ const mapHeight int = 100
 const botNumber int = 50
 const zombieCutoff int = 13
 const weaponStrength int = 6
+const playerMinAttack int = 1
+const playerMaxAttack int = 6
 const turnLength int = 60
 const idSalt string = "6LIBN8OWPzTKctUvbZtXV2mFn2tCq3qZKjHYbTTnLWtu6oGTU3ow3tuNx9SBTuND"
