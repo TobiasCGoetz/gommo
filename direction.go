ac41d01c48371663e5e56615a37e027f21fae170
TobiasCGoetz <tskaloss@posteo.de>
2025-01-21 21:41:51 +0100

    Make new player movement direction configurable, don't overwrite after each turn

--- direction.go
+++ direction.go
@@ -1,5 +1,7 @@
 package main
 
+import "encoding/json"
+
 type Direction int
 
 const (
@@ -14,6 +16,10 @@ var Directions = [5]Direction{North, East, South, West, Stay}
 
 func (d Direction) toString() string {
 	return []string{"North", "East", "South", "West", "Stay"}[d]
+}
+
+func (d Direction) MarshalJSON() ([]byte, error) {
+	return json.Marshal(d.toString())
 }
 
 var directions = map[string]Direction{
