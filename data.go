7d71e0be34c89c40c1020d41d63c2c81a0a44b24
TobiasCGoetz <tskaloss@posteo.de>
2025-01-21 21:41:45 +0100

    Initial commit with some data structures

---
+++ data.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"strings"
+	"fmt"
+)
+
+//Tile
+type Tile int
+const (
+	Forest Tile = iota
+	Farm
+	City
+	Laboratory
+)
+
+func (t Tile) String() string {
+	return []string{"Forest", "Farm", "City", "Laboratory"}[t]
+}
+
+//Cards
+type Card int
+
+const (
+	Food Card = iota
+	Wood
+	Weapon
+	None
+)
+
+func (c Card) String() string {
+	return []string{"Food", "Wood", "Weapon", "None"}[c]
+}
+
+//Players
+
+type Player struct {
+	id string
+	x, y int
+	cards [4]Card
+}
+
+func (p Player) toString() string {
+	var r strings.Builder
+	r.WriteString(p.id)
+	r.WriteString(": ")
+	r.WriteString(fmt.Sprintf("%d", p.x))
+	r.WriteString("|")
+	r.WriteString(fmt.Sprintf("%d", p.y))
+	r.WriteString(" ")
+	r.WriteString(p.cards[0].String())
+	r.WriteString(p.cards[1].String())
+	r.WriteString(p.cards[2].String())
+	r.WriteString(p.cards[3].String())
+	return r.String()
+}
