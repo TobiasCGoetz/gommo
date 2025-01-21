9f171b415d5754bb3a962875b3308cc13e515650
TobiasCGoetz <tskaloss@posteo.de>
2025-01-21 21:41:45 +0100

    Fixing up go syntax for data structures

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 )
 
 func main() {
-	var forest Tile = Tile.Forest
-	var food Card = Card.Food
-	var me Player = Player{"first", 13, 42, Card.Food, Card.Wood, Card.Wood, Card.None}
-	fmt.Println(me.String())
+	var forest Tile = Forest
+	var food Card = Food
+	var me Player = Player{"first", 13, 42, [4]Card{Food, Wood, Wood, None}}
+	fmt.Println(forest.toString())
+	fmt.Println(food.toString())
+	fmt.Println(me.toString())
 }
