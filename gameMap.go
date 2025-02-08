d036273f197fee08c622e39a3a2ceb846e65fc02
TobiasCGoetz <tskalloss@posteo.de>
2025-02-08 13:25:58 +0100

    Prepare tile funcs for the transition to storing only playerIds in tiles

--- gameMap.go
+++ gameMap.go
@@ -4,7 +4,31 @@ type Tile struct {
 	Terrain Terrain
 	Zombies int
 	Players []Player
+	//playerIds []string
 }
+
+/*
+func (t *Tile) addPlayer(incomingPlayer string) {
+	t.playerIds = append(t.playerIds, incomingPlayer)
+}
+
+func (t *Tile) removePlayer(leavingPlayer string) {
+	index, found := t.findPlayerIdIndex(leavingPlayer)
+	if found {
+		t.playerIds = append(t.playerIds[:index], t.playerIds[index+1])
+	}
+}
+
+func (t Tile) findPlayerIdIndex(playerId string) (int, bool) {
+	for index, pId := range t.playerIds {
+		if pId == playerId {
+			return index, true
+		}
+	}
+	return -1, false
+}
+
+*/
 
 type MapPiece struct {
 	TileType             string
