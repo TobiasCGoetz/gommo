fbd03e44f9e8e0262037d4608c76d82d0e551aab
TobiasCGoetz <tskaloss@posteo.de>
2025-03-18 20:19:07 +0100

    Slim down base event

--- event.go
+++ event.go
@@ -5,14 +5,12 @@ import "time"
 type Event interface {
 	ID() string
 	Timestamp() time.Time
-	Payload() any
 	Type() string
 }
 
 type BaseEvent struct {
 	id        string
 	timestamp time.Time
-	payload   any
 	eventType string
 }
 
@@ -26,17 +24,11 @@ func (e BaseEvent) Timestamp() time.Time {
 	return e.timestamp
 }
 
-// Payload returns the event payload.
-func (e BaseEvent) Payload() interface{} {
-	return e.payload
-}
-
 // Type returns the event type.
 func (e BaseEvent) Type() string {
 	return e.eventType
 }
 
-// Example event struct
 type UserCreated struct {
 	BaseEvent
 	Username string
