38e734fabca59b6ab039935884aa0d66f4de63dc
TobiasCGoetz <tskaloss@posteo.de>
2025-03-18 21:19:11 +0100

    Simplify event layout for now

--- event.go
+++ event.go
@@ -2,13 +2,6 @@ package main
 
 import "time"
 
-type Event interface {
-	ID() string
-	Timestamp() time.Time
-	Type() string
-	Succeded() bool
-}
-
 type BaseEvent struct {
 	id        string
 	timestamp time.Time
@@ -16,27 +9,12 @@ type BaseEvent struct {
 	success   bool
 }
 
-// ID returns the event ID.
-func (e BaseEvent) ID() string {
-	return e.id
-}
-
-// Timestamp returns the event timestamp.
-func (e BaseEvent) Timestamp() time.Time {
-	return e.timestamp
-}
-
-// Type returns the event type.
-func (e BaseEvent) Type() string {
-	return e.eventType
-}
-
-// Succeeded returns the success indicator
-func (e BaseEvent) Succeeded() bool {
-	return e.success
-}
-
-type UserCreated struct {
+type WriteUser struct {
 	BaseEvent
 	Username string
+}
+
+type ReadUser struct {
+	BaseEvent
+	Player Player
 }
