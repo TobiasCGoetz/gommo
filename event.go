32805c16cdd2c4d5fe9449723857aac0db93762f
TobiasCGoetz <tskaloss@posteo.de>
2025-03-20 22:33:15 +0100

    (Re-)introduce event interface and extend BaseEvent

--- event.go
+++ event.go
@@ -2,11 +2,29 @@ package main
 
 import "time"
 
+type Event interface {
+	Type() string
+	Timestamp() time.Time
+	OfPlayer() string
+}
+
 type BaseEvent struct {
-	Id        string
-	Timestamp time.Time
-	EventType string
+	playerId  string
+	timestamp time.Time
+	eventType string
 	Success   bool
+}
+
+func (event BaseEvent) OfPlayer() string {
+	return event.playerId
+}
+
+func (event BaseEvent) Type() string {
+	return event.eventType
+}
+
+func (event BaseEvent) Timestamp() time.Time {
+	return event.timestamp
 }
 
 type WriteUser struct {
