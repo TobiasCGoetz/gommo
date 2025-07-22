42d2d83326da9da2eb043f191ec6b49a4f396c20
TobiasCGoetz <tskaloss@posteo.de>
2025-07-22 18:31:10 +0200

    Add broad tests as TODO outline, no function body yet

---
+++ main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+)
+
+// We have the following distinct test cases:
+// Movement
+// Combat x 2 (Win + Lose)
+// Consume
+// Resource Distribution
+// Win
+
+func TestMove (t *testing.T) {}
+func TestCombatLose (t *testing.T) {}
+func TestCombatWin (t *testing.T) {}
+func TestConsume (t *testing.T) {}
+func TestResources (t *testing.T) {}
+func TestWin (t *testing.T) {}
